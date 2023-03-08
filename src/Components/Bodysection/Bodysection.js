7fa5cca07e3f8a91b13b65f68922ca4a95c498fd
pradeep <pradeep.kunchala15@gmail.com>
2023-03-08 19:51:38 +0530

    first commit

---
+++ src/Components/Bodysection/Bodysection.js
@@ -0,0 +1,137 @@
+import React from "react";
+import "./Bodysection.css";
+const Bodysection = () => {
+  return (
+    <div>
+      <section id="hero">
+        <div class="hero-container" data-aos="fade-up">
+          <h1>Welcome to Serenity</h1>
+          <h2>
+            We are team of talented designers making websites with Bootstrap
+          </h2>
+          <a href="#about" class="btn-get-started scrollto">
+            Get Started
+          </a>
+        </div>
+      </section>
+
+      <main id="main"></main>
+
+      <section id="about" class="about">
+        <div class="container">
+          <div class="row justify-content-end">
+            <div class="col-lg-11">
+              <div class="row justify-content-end">
+                <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                  <div class="count-box py-5">
+                    <i class="bi bi-emoji-smile"></i>
+                    <span
+                      data-purecounter-start="0"
+                      data-purecounter-end="65"
+                      class="purecounter"
+                    >
+                      0
+                    </span>
+                    <p>Happy Clients</p>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                  <div class="count-box py-5">
+                    <i class="bi bi-journal-richtext"></i>
+                    <span
+                      data-purecounter-start="0"
+                      data-purecounter-end="85"
+                      class="purecounter"
+                    >
+                      0
+                    </span>
+                    <p>Projects</p>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                  <div class="count-box pb-5 pt-0 pt-lg-5">
+                    <i class="bi bi-clock"></i>
+                    <span
+                      data-purecounter-start="0"
+                      data-purecounter-end="27"
+                      class="purecounter"
+                    >
+                      0
+                    </span>
+                    <p>Years of experience</p>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                  <div class="count-box pb-5 pt-0 pt-lg-5">
+                    <i class="bi bi-award"></i>
+                    <span
+                      data-purecounter-start="0"
+                      data-purecounter-end="22"
+                      class="purecounter"
+                    >
+                      0
+                    </span>
+                    <p>Awards</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-6 video-box align-self-baseline position-relative">
+              <img src="assets/img/about.jpg" class="img-fluid" alt=""></img>
+              <a
+                href="https://www.youtube.com/watch?v=jDDaplaOz7Q"
+                class="glightbox play-btn mb-4"
+              ></a>
+            </div>
+
+            <div class="col-lg-6 pt-3 pt-lg-0 content">
+              <h3>
+                Voluptatem dignissimos provident quasi corporis voluptates sit
+                assumenda.
+              </h3>
+              <p class="fst-italic">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <ul>
+                <li>
+                  <i class="bx bx-check-double"></i> Ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </li>
+                <li>
+                  <i class="bx bx-check-double"></i> Duis aute irure dolor in
+                  reprehenderit in voluptate velit.
+                </li>
+                <li>
+                  <i class="bx bx-check-double"></i> Voluptate repellendus
+                  pariatur reprehenderit corporis sint.
+                </li>
+                <li>
+                  <i class="bx bx-check-double"></i> Ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate trideta storacalaperda mastiro
+                  dolore eu fugiat nulla pariatur.
+                </li>
+              </ul>
+              <p>
+                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Bodysection;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Currency Exchange</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features"></Nav.Link>
+              <Nav.Link href="#pricing"></Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Home</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                About
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
