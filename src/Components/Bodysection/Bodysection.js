5fa8c0ca479bb69ec2eb54d44e7efbf2d7960249
pradeep <pradeep.kunchala15@gmail.com>
2023-03-10 01:16:57 +0530

    added body section

--- src/Components/Bodysection/Bodysection.js
+++ src/Components/Bodysection/Bodysection.js
@@ -1,23 +1,99 @@
 import React from "react";
 import "./Bodysection.css";
-const Bodysection = () => {
+function Bodysection() {
   return (
     <div>
-      <div className="form-container">
-        <div className="text-container">
-          <h1>Welcome to My Form</h1>
-          <p>Please fill out the fields below:</p>
+      <section id="hero">
+        <div className="hero-container" data-aos="fade-up">
+          <h1>Welcome to Currency Exchange</h1>
+          <h2>
+            We are team of talented designers making websites with Bootstrap
+          </h2>
         </div>
-        <form className="my-form">
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-          <label htmlFor="message">Message:</label>
-        </form>
-      </div>
+      </section>
+
+      <main id="main">
+        <section id="about" className="about">
+          <div className="container">
+            <div className="row justify-content-end">
+              <div className="col-lg-11">
+                <div className="row justify-content-end">
+                  <div className="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box py-5">
+                      <i className="bi bi-emoji-smile"></i>
+                      <span
+                        data-purecounter-start="0"
+                        data-purecounter-end="65"
+                        className="purecounter"
+                      >
+                        0
+                      </span>
+                      <p>Happy Clients</p>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box py-5">
+                      <i className="bi bi-journal-richtext"></i>
+                      <span
+                        data-purecounter-start="0"
+                        data-purecounter-end="85"
+                        className="purecounter"
+                      >
+                        0
+                      </span>
+                      <p>Projects</p>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box pb-5 pt-0 pt-lg-5">
+                      <i className="bi bi-clock"></i>
+                      <span
+                        data-purecounter-start="0"
+                        data-purecounter-end="27"
+                        className="purecounter"
+                      >
+                        0
+                      </span>
+                      <p>Years of experience</p>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box pb-5 pt-0 pt-lg-5">
+                      <i className="bi bi-award"></i>
+                      <span
+                        data-purecounter-start="0"
+                        data-purecounter-end="22"
+                        className="purecounter"
+                      >
+                        0
+                      </span>
+                      <p>Awards</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-lg-6 video-box align-self-baseline position-relative">
+                <img src=" " className="img-fluid" alt=""></img>
+              </div>
+
+              <div className="col-lg-6 pt-3 pt-lg-0 content">
+                <h3>
+                  Voluptatem dignissimos provident quasi corporis voluptates sit
+                  assumenda.
+                </h3>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
-};
+}
 
 export default Bodysection;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
+/*import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
@@ -11,19 +11,50 @@ const Header = () => {
           <Navbar.Brand href="#home">Currency Exchange</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
+            <Nav classNameName="me-auto">
               <Nav.Link href="#features"></Nav.Link>
               <Nav.Link href="#pricing"></Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">Home</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                About
-              </Nav.Link>
+              <Nav.Link href="#deets"></Nav.Link>
+              <Nav.Link eventKey={2} href="#memes"></Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+    </div>
+  );
+};*/
+
+const Header = () => {
+  return (
+    <div>
+      <header id="header" className="fixed-top d-flex align-items-center">
+        <div className="container d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <h1 className="text-light">
+              <a href="index.html">Currency Exchange</a>
+            </h1>
+            <a href="index.html">
+              <img src="assets/img/logo.png" alt="" className="img-fluid"></img>
+            </a>
+          </div>
+
+          <nav id="navbar" className="navbar">
+            <ul>
+              <li>
+                <a className="active">Home</a>
+              </li>
+              <li className="dropdown">
+                <a>
+                  <span></span> <i className="bi bi-chevron-down"></i>
+                </a>
+              </li>
+            </ul>
+            <i className="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+        </div>
+      </header>
     </div>
   );
 };
